2aa9314f1651389901bd4d52a4e879ff37426dd9
seongho9 <seongho9276@gmail.com>
2024-08-01 17:42:39 +0900

    fix converther.h

--- veda_cpp/veda_cpp/UserTodoConverter.h
+++ veda_cpp/veda_cpp/UserTodoConverter.h
@@ -1,12 +1,26 @@
 #ifndef _USER_TODO_CONVERT_H
+#define _USET_TODO_CONVERT_H
 #include <vector>
 #include <map>
+#include "TodoRepo.h"
 #include "UserTodo.h"
 #include "Todo.h"
 
 class UserTodoConverter
 {
-	virtual std::vector<UserTodo> load() = 0;
+	virtual bool load(std::map<std::string, UserTodo>& res) = 0;
 	virtual unsigned int save(std::map<std::string, UserTodo> data) = 0;
+};
+
+class UserTodoConverterImpl : public UserTodoConverter
+{
+	TodoRepo* repo;
+
+	void calculateFinish(std::vector<Todo> data, std::vector<Todo> finish);
+public:
+	UserTodoConverterImpl(TodoRepo* repo);
+
+	bool load(std::map<std::string, UserTodo>& res) override;
+	unsigned int save(std::map<std::string, UserTodo> data) override;
 };
 #endif // !_USER_TODO_CONVERT_H
