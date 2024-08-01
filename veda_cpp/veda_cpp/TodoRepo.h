c06268517bd13ed0d8abf25172c6031cb1cecb5a
seongho9 <seongho9276@gmail.com>
2024-08-01 16:10:58 +0900

    TodoRepo interface fix

--- veda_cpp/veda_cpp/TodoRepo.h
+++ veda_cpp/veda_cpp/TodoRepo.h
@@ -34,30 +34,32 @@ public:
 	/// 프로그램 시작 시 영구 저장소의 Todo 데이터 모두 가져옴
 	/// </summary>
 	/// <returns> Todo 데이터의 배열</returns>
-	virtual std::vector<Todo>& load() = 0;
+	virtual void load(std::vector<Todo>& res) = 0;
 	/// <summary>
 	/// Todo 데이터를 수정
 	/// </summary>
 	/// <param name="id"> 수정하고자 하는 Todo의 id</param>
 	/// <param name="data">수정하고자하는 완전한 Todo 객체</param>
 	/// <returns> 저장한 Todo객체</returns>
-	virtual Todo& modify(unsigned int id, Todo data) = 0;
+	virtual bool modify(unsigned int id, Todo data) = 0;
 };
 
 class TodoRepoFile : public TodoRepo
 {
 private:
-	std::fstream file;
+	std::ofstream output;
+	std::ifstream input;
+	std::string dir;
 	std::mutex lock;
-
 public:
 
+	TodoRepoFile();
 	Todo& save(Todo& data);
 
 	unsigned int saveArray(std::vector<Todo> data);
 	unsigned int deleteById(unsigned int id);
-	std::vector<Todo>& load();
-	Todo& modify(unsigned int id, Todo data);
+	void load(std::vector<Todo>& res);
+	bool modify(unsigned int id, Todo data);
 
 };
 #endif // !_TODO_REPO_H
