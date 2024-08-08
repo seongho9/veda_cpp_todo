c7fa4c522a2d889fb9c21fd1914481085802b7ce
seongho9 <seongho9276@gmail.com>
2024-08-08 16:34:30 +0900

    [FIX] remove save, delete, modify method

--- veda_cpp/veda_cpp/TodoRepoFile.cpp
+++ veda_cpp/veda_cpp/TodoRepoFile.cpp
@@ -9,18 +9,6 @@ TodoRepoFile::TodoRepoFile()
 	dir = "./data";
 }
 
-Todo& TodoRepoFile::save(Todo& data)
-{
-	lock.lock();
-
-	output.open(dir, ios::out | ios::binary);
-	output.write((char*)&data, sizeof(Todo));
-	output.close();
-	lock.unlock();
-
-
-	return data;
-}
 
 unsigned int TodoRepoFile::saveArray(vector<Todo> data)
 {
@@ -35,11 +23,6 @@ unsigned int TodoRepoFile::saveArray(vector<Todo> data)
 	lock.unlock();
 
 	return i;
-}
-
-unsigned int TodoRepoFile::deleteById(unsigned int id)
-{
-	return 0;
 }
 
 void TodoRepoFile::load(vector<Todo>& res)
@@ -76,32 +59,4 @@ void TodoRepoFile::load(vector<Todo>& res)
 	input.close();
 	lock.unlock();
 
-}
-
-bool TodoRepoFile::modify(unsigned int id, Todo data)
-{
-	lock.lock();
-
-	input.open(dir, ios::binary);
-	input.seekg(ios::beg);
-
-	while (!input.eof()) {
-		Todo *buf = new Todo();
-		input.read(reinterpret_cast<char*>(buf), sizeof(Todo));
-
-		if (buf->getId() == id) {
-			streampos idx = input.tellg();
-			idx -= sizeof(Todo);
-			input.close();
-			output.open(dir, ios::out | ios::binary);
-			output.seekp(idx);
-			output.write(reinterpret_cast<char*>(&data), sizeof(Todo));
-			output.close();
-			return true;
-		}
-		delete buf;
-	}
-	Todo fail = Todo();
-	data.setUserName("");
-	return false;
 }
